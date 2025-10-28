81d365268407a832ae11f2c89c163b5a3dc1efe5
Kris-Ekiteles <krisekteles@gmail.com>
2025-10-28 13:12:11 +0300

    input form

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import './Home.css'
 const Registration = () => {
   return (
     <div>
       <h1>welcome and feel free to register</h1>
+      <form action="#">
+        <div className="form-group">
+          <h2>Fill out the form bellow</h2>
+        <label for="name">PAtient Name</label>
+        <input type="text" placeholder='enter Patient name' id='name' />
+        <input type="text" placeholder='enter last name' id='name' />
+        <label for="age">Age</label>
+        <input type="number" placeholder='enter patients age' id='age'/>
+        <label for="alergy">Alargies</label>
+        <input type="text" placeholder='Any alergies should be provided here' id='alergy'/>
+        <button>Submit</button>
+        </div>
+      </form>
+          
     </div>
-  )
+  ) 
 }
 
 export default Registration
